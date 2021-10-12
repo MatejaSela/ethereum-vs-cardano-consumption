783ba96b123ed82456e3b83ac8bc67d81afafe84
Mateja Sela <mateja.sela@thoughtworks.com>
2021-10-12 17:11:29 +0200

    create the formulae and add text to describe the calculation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,21 @@ import React from 'react';
 import './index.css';
 
 function App() {
+  const TP = 3083;
+  const RPP = 3;
+  
+  const MS = 265; // src: https://outervision.com/power-supply-calculator
+  const numberOfHoursPerYear = 8760;
+  const UpConstant = 0.1;
+
+  const totalPerYearSingleNode = MS*numberOfHoursPerYear;
+  const totalPerYearkWhSingleNode = MS*numberOfHoursPerYear/1000;
+
+  const finalCalculationkWh = TP*RPP*MS + TP*RPP*MS * 0.1;
+  const finalCalculationMWh = (TP*RPP*MS + TP*RPP*MS * 0.1)/1000;
+
+  const lastNumberEthereum = 77.26755054*1000000; // TODO pull from CSV
+
   return (
    
     <div>
@@ -16,23 +31,44 @@ function App() {
       <br/>
 
       <p className="textfield"> The following graph presents the Ethereum electricity consumption in TWh. Ethereum is the major blockhain where
-      most artists decide to mint their NFTs, which is relevant to a high-quality platform that is Artano</p>
+      most artists decide to mint their NFTs, and we're here to show that there's a more sustainable alternative</p>
 
       <EthereumChart />
 
-      <p className="textfield"> As Ethereum uses a proof of work algorithm to execute its transacitons (which includes transactions
-        when you buy or sell NFTs on an Ethereum platform, it requires enormous amounts of energy to execute a single transaction, which has accumulated to
-        energy consumption of countries like Bangladesh, a top 40 consumer. </p>
+      <p className="textfield"> As Ethereum uses a proof of work algorithm to execute its transacitons, it requires enormous amounts of energy to execute a single transaction, which has accumulated to
+        energy consumption of countries like Bangladesh, a top 40 consumer on a yearly basis. </p>
 
       <h1 className="header">It doesn't have to be this way! </h1>
 
 
-      <p className="textfield"> Cardano uses a proof of Stake of algorithm, a much more sustainable alternative to proof of work, which Bitcoin and Ethereum use.
-      Since there is currently no data on the overall transaction of the Cardano blockchain, 
-      we will take the minimum specs required to run a node (meaning a computer that is able to make transactions) </p>
+      <p className="textfield"> There are algorithms like proof of history or proof of stake that offer a more sustainable solution than Ethereum.
+      Cardano uses a proof of Stake of algorithm.</p>
       <br/>
-      <p className="textfield"> We've used the minimum system requirements, meaning RAM, space and computing power required to successfully produce a block on Cardano. You can check these
-    here[LINK]. </p>
+
+      <p className="textfield"> How sustainable is proof of stake though? Since there is currently no data on the overall transaction of the Cardano blockchain, 
+      we will take the minimum specs required to run a node, meaning a computer that is able to make transactions on Cardano - a computer
+      which can mint, sell and buy NFTs from any Cardano marketplace. </p>
+
+      <br/>
+      <p className="textfield"> We've used the minimum system requirements, meaning RAM, number of cores and CPU needed to successfully produce a block on Cardano. 
+      You can check these <a href="https://github.com/input-output-hk/cardano-node/releases" target="_blank">here</a>. 
+      
+      </p>
+
+    <h2 className="formula">Now for the formula we used: </h2>
+    <br/>
+    <p className="formula">We take the total number of pools (Total Pools = TP = {TP})</p>
+    <p className="formula">Assume the average number of relays is 3 per pool (Relays Per Pool = RPP = {RPP})</p>
+    <p className="formula">The minimum system requirements are 8GB, 2 cores, 2GHz at the moment (Minimum Specs Consumption = MS = {MS}W)</p>
+    <p className="formula">Total consumption per year is MS * Number of hours per year (8760h) = {totalPerYearSingleNode} Wh = {totalPerYearkWhSingleNode} kWh</p>
+    <p className="formula">Up the minimum requirements by 10%, as most relays run on more powerful machines (Up Constant = {UpConstant*100}%)</p>
+
+    <br/>
+    <p className="formula"> Therefore the total Cardano consumption = TP*RPP*MS + TP*RPP*MS * 10% = {finalCalculationkWh} kWh</p> 
+    <p className="formula"> {finalCalculationkWh}kWh is equivalent to {finalCalculationMWh}MWh</p> 
+
+    <p className="textfield"> Cardano is currently {lastNumberEthereum / finalCalculationMWh} times more energy efficient than Ethereum</p>. 
+    
     </div> 
   );
 }
