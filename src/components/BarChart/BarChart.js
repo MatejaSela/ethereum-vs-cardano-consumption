583d5a415aee4b5c1ed0ed796af4dfbe4bee773c
Mateja Sela <mateja.sela@thoughtworks.com>
2021-10-20 11:52:19 +0200

    y axis changes, changed labels to years

--- src/components/BarChart/BarChart.js
+++ src/components/BarChart/BarChart.js
@@ -5,8 +5,8 @@ import React, { useState, useEffect } from "react";
 
 const BarChart = (props) => {
     const [data, setData] = useState([
-        {group: "Cardano " + 1 + " tx", value: props.totCardano},
-        {group: "Ethereum 1tx", value: props.totEthereum},
+        {group: "Cardano", value: props.totCardano},
+        {group: "Ethereum", value: props.totEthereum},
     ]);
 
     // set the dimensions and margins of the graph
@@ -16,26 +16,30 @@ const BarChart = (props) => {
 
     // create a tooltip
 
+
     var mouseover = function(d) {
+        let eth_cons = data[1].value
+        let ada_cons = data[0].value
+        let text;
+        if(eth_cons > ada_cons){
+            text = "The selected transaction number is " + data[1].value/data[0].value + " times less energy consuming than Ethereum"
+        }
+        else{
+            text = "The selected transaction number is " + data[1].value/data[0].value + " times more energy consuming than Ethereum"
+        }
         d3.select(".barwrapper")
             .append("div")
                 .attr("class", "tooltip")
                 .style("visibility", "visible")
                 .style("opacity", 1)
                 .style("background-color", "white")
-                .style("border", "solid")
-                .style("border-width", "2px")
-                .style("border-radius", "5px")
-                .style("padding", "5px")
                 .append("p")
                     .attr("class", "texty")
-                    .text('hello')
-
-        console.log("here")
+                    .text(text)
         
         d3.select(this)
-        .style("stroke", "black")
-        .style("opacity", 1)
+            .style("stroke", "black")
+            .style("opacity", 1)
     }
 
     var mouseleave = function(d) {
@@ -43,7 +47,7 @@ const BarChart = (props) => {
         d3.select(".tooltip")
             .attr("class", ".tooltip")
             .style("visibility", "hidden")
-            .remove("p").attr("class", "texty").text('hello')
+            .remove("p").attr("class", "texty")
 
         d3.select(this)
             .style("stroke", "none")
@@ -57,18 +61,10 @@ const BarChart = (props) => {
     .append("g")
     .attr("transform", `translate(${margin.left},${margin.top})`)
 
-    // X-axis label
-
-    svg.append("text")
-        .attr("class", "x label")
-        .attr("x", width)
-        .attr("y", height - 6)
-        .text("Transactions");
-
     function handleChange(e) {
         const data1 = [
-            {group: "Cardano " + e.target.value + " tx", value: props.totCardano*e.target.value},
-            {group: "Ethereum 1tx", value: props.totEthereum},
+            {group: "Cardano", value: props.totCardano*e.target.value},
+            {group: "Ethereum", value: props.totEthereum},
         ];
         setData(data1)
         svg.remove("svg")
@@ -77,36 +73,45 @@ const BarChart = (props) => {
     useEffect(() => {
         update(data)
     }, [data]);
+        
+    function update(data) {
 
-    // X axis
-    const x = d3.scaleBand()
+        // X-axis label
+        svg.append("text")
+        .attr("class", "x label")
+        .attr("x", width)
+        .attr("y", height - 6)
+        .text("Blockchain");
+
+        // X axis
+        const x = d3.scaleBand()
         .range([0, width ])
         .domain(data.map(d => d.group))
         .padding(0.2);
 
-    svg.append("g")
-        .attr("transform", `translate(0,${height})`)
-        .call(d3.axisBottom(x));
+        svg.append("g")
+            .attr("transform", `translate(0,${height})`)
+            .call(d3.axisBottom(x));
 
-    // Y-axis label   
-    svg.append("text")
-        .attr("class", "y label")
-        .attr("text-anchor", "end")
-        .attr("y", 6)
-        .attr("dy", ".75em")
-        .attr("transform", "rotate(-90)")
-        .text("Consumption in MWh");
-        
-    // Add Y axis
-    const y = d3.scaleLinear()
-        .domain([0, props.totEthereum])
-        .range([ height, 0]);
+        // Y-axis label   
+        svg.append("text")
+            .attr("class", "y label")
+            .attr("text-anchor", "end")
+            .attr("y", 6)
+            .attr("dy", ".75em")
+            .attr("transform", "rotate(-90)")
+            .text("Consumption in MWh");
+            
+        let y = props.totEthereum > data[0].value ? d3.scaleLinear()
+            .domain([0, props.totEthereum])
+            .range([ height, 0]) : d3.scaleLinear()
+            .domain([0, data[0].value])
+            .range([ height, 0])
 
-    svg.append("g")
-        .attr("class", "myYaxis")
-        .call(d3.axisLeft(y));
-        
-    function update(data) {
+        svg.append("g")
+            .attr("class", "myYaxis")
+            .call(d3.axisLeft(y));
+
         var u = svg.selectAll("rect")
             .data(data)
             
@@ -124,8 +129,8 @@ const BarChart = (props) => {
         <div className="barwrapper"> 
         <form>
         <label>
-            Cardano transactions:
-            <input name="Cardano Transactions" onChange={handleChange} /> transactions
+            Cardano years of consumption:
+            <input name="Cardano Transactions" onChange={handleChange} />
         </label>
         </form>
         {data ?     
